17f78714da81c26f2fd64d503166fd9dedbe93cc
tom <tomnelson@users.noreply.github.com>
2022-03-28 15:06:03 +0000

    the fix is in

--- warehouse/query-core/src/test/java/datawave/query/config/ShardQueryConfigurationTest.java
+++ warehouse/query-core/src/test/java/datawave/query/config/ShardQueryConfigurationTest.java
@@ -478,7 +478,7 @@ public class ShardQueryConfigurationTest {
      */
     @Test
     public void testCheckForNewAdditions() throws IOException {
-        int expectedObjectCount = 188;
+        int expectedObjectCount = 191;
         ShardQueryConfiguration config = ShardQueryConfiguration.create();
         ObjectMapper mapper = new ObjectMapper();
         JsonNode root = mapper.readTree(mapper.writeValueAsString(config));
