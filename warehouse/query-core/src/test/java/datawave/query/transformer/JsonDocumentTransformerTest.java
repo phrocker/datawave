376ec855f2b5d3e23670c6db864c8e8900ab79f7
phrocker <phrocker@apache.org>
2022-05-21 10:20:17 +0000

    Update test and fix powermock issues

--- warehouse/query-core/src/test/java/datawave/query/transformer/JsonDocumentTransformerTest.java
+++ warehouse/query-core/src/test/java/datawave/query/transformer/JsonDocumentTransformerTest.java
@@ -1,5 +1,7 @@
 package datawave.query.transformer;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 import datawave.marking.MarkingFunctions;
 import datawave.query.Constants;
 import datawave.query.DocumentSerialization;
@@ -8,6 +10,7 @@ import datawave.query.attributes.Numeric;
 import datawave.query.function.deserializer.KryoDocumentDeserializer;
 import datawave.query.function.serializer.DocumentSerializer;
 import datawave.query.tables.document.batch.DocumentLogic;
+import datawave.query.tables.serialization.JsonDocument;
 import datawave.query.tables.serialization.SerializedDocument;
 import datawave.query.tables.serialization.SerializedDocumentIfc;
 import datawave.webservice.query.Query;
@@ -37,7 +40,8 @@ import java.util.List;
 import java.util.Map;
 
 @RunWith(PowerMockRunner.class)
-@PowerMockIgnore("javax.management.*")
+//@PowerMockIgnore({"javax.management.*","jdk.xml.*"})
+@PowerMockIgnore({"com.sun.org.apache.xerces.*", "javax.xml.*", "org.xml.*", "javax.management.*"})
 @PrepareForTest({JsonDocumentTransformer.class, DocumentSerialization.class})
 public class JsonDocumentTransformerTest { // extends EasyMockSupport {
     private JsonDocumentTransformer transformer;
@@ -68,7 +72,7 @@ public class JsonDocumentTransformerTest { // extends EasyMockSupport {
         
     }
     
-    private void basicExpects(Document d, Key key, Map.Entry<Key,Value> entry) throws MarkingFunctions.Exception {
+    private void basicExpects(Document d, Key key, Map.Entry<Key,Value> entry, int translateVisibilityCalls) throws MarkingFunctions.Exception {
         // AbstractMap.SimpleEntry<Key, Document> documentEntry = new AbstractMap.SimpleEntry<>(key, mockDocument);
         d.put("field1", new Numeric("5", key, true));
         AbstractMap.SimpleEntry<Key,Document> documentEntry = new AbstractMap.SimpleEntry<>(key, d);
@@ -86,33 +90,88 @@ public class JsonDocumentTransformerTest { // extends EasyMockSupport {
         EasyMock.expect(mockLogic.getTableName()).andReturn("table1");
         EasyMock.expect(mockQuery.getQueryAuthorizations()).andReturn("A,B,C");
         EasyMock.expect(mockQuery.findParameter("log.timing.details")).andReturn(new QueryImpl.Parameter("", ""));
-        EasyMock.expect(mockDeserializer.apply(entry)).andReturn(documentEntry);
+
         // EasyMock.expect(mockDocument.getDictionary()).andReturn(Collections.EMPTY_MAP);
         // mockDocument.debugDocumentSize(key);
-        EasyMock.expect(mockMarkingFunctions.translateFromColumnVisibility(key.getColumnVisibilityParsed())).andReturn(Collections.EMPTY_MAP).times(2);
+        EasyMock.expect(mockMarkingFunctions.translateFromColumnVisibility(key.getColumnVisibilityParsed())).andReturn(Collections.EMPTY_MAP).times(translateVisibilityCalls);
         // EasyMock.expect(mockDocument.getDictionary()).andReturn(dictionary);
         // EasyMock.expect(mockNumeric.getData()).andReturn("5");
         EasyMock.expect(mockResponseFactory.getField()).andReturn(simpleField);
         EasyMock.expect(mockResponseFactory.getEvent()).andReturn(simpleEvent);
         // EasyMock.expect(mockDocument.sizeInBytes()).andReturn(1l);
     }
-    
+
+    private void basicExpects(JsonObject d, Key key, Map.Entry<Key,Value> entry, int translateVisibilityCalls) throws MarkingFunctions.Exception {
+        // AbstractMap.SimpleEntry<Key, Document> documentEntry = new AbstractMap.SimpleEntry<>(key, mockDocument);
+
+        d.add("field1", new JsonPrimitive(5));
+        //AbstractMap.SimpleEntry<Key,Document> documentEntry = new AbstractMap.SimpleEntry<>(key, d);
+
+        // Map<String,Attribute<?extends Comparable<?>>> dictionary = new HashMap<>();
+        // dictionary.put("field1", mockNumeric);
+
+        SimpleField simpleField = new SimpleField();
+        SimpleEvent simpleEvent = new SimpleEvent();
+
+        PowerMock.mockStatic(DocumentSerialization.class);
+
+        EasyMock.expect(DocumentSerialization.getDocumentDeserializer(mockQuery)).andReturn(mockDeserializer);
+
+        EasyMock.expect(mockLogic.getTableName()).andReturn("table1");
+        EasyMock.expect(mockQuery.getQueryAuthorizations()).andReturn("A,B,C");
+        EasyMock.expect(mockQuery.findParameter("log.timing.details")).andReturn(new QueryImpl.Parameter("", ""));
+
+        // EasyMock.expect(mockDocument.getDictionary()).andReturn(Collections.EMPTY_MAP);
+        // mockDocument.debugDocumentSize(key);
+        EasyMock.expect(mockMarkingFunctions.translateFromColumnVisibility(key.getColumnVisibilityParsed())).andReturn(Collections.EMPTY_MAP).times(translateVisibilityCalls);
+        // EasyMock.expect(mockDocument.getDictionary()).andReturn(dictionary);
+        // EasyMock.expect(mockNumeric.getData()).andReturn("5");
+        EasyMock.expect(mockResponseFactory.getField()).andReturn(simpleField);
+        EasyMock.expect(mockResponseFactory.getEvent()).andReturn(simpleEvent);
+        // EasyMock.expect(mockDocument.sizeInBytes()).andReturn(1l);
+    }
+
     @Test
     public void transform_noPrimaryToSecondaryMapSetTest() throws MarkingFunctions.Exception {
         Key key = new Key("shard", "dataType" + Constants.NULL + "uid");
         Value value = new Value();
         AbstractMap.SimpleEntry<Key,Value> entry = new AbstractMap.SimpleEntry<>(key, value);
         Document d = new Document(key,true);
-        basicExpects(d, key, entry);
+        basicExpects(d, key, entry,2);
 
         SerializedDocument doc = new SerializedDocument(d);
         PowerMock.replayAll();
         
         transformer = new JsonDocumentTransformer(mockLogic, mockQuery, mockMarkingFunctions, mockResponseFactory, true);
         SimpleEvent event = (SimpleEvent) transformer.transform(doc);
+        transformer.flush();
         
         PowerMock.verifyAll();
         
+        Assert.assertNotNull(event);
+        Assert.assertEquals(1, event.getFields().size());
+        Assert.assertEquals("field1", event.getFields().get(0).getName());
+        Assert.assertEquals("5", event.getFields().get(0).getValueString());
+    }
+
+    @Test
+    public void transform_noPrimaryToSecondaryMapSetTestJson() throws MarkingFunctions.Exception {
+        Key key = new Key("shard", "dataType" + Constants.NULL + "uid");
+        Value value = new Value();
+        AbstractMap.SimpleEntry<Key,Value> entry = new AbstractMap.SimpleEntry<>(key, value);
+
+        JsonObject d= new JsonObject();
+        basicExpects(d, key, entry,2);
+
+        JsonDocument doc = new JsonDocument(d,key.toThrift(),1);
+        PowerMock.replayAll();
+
+        transformer = new JsonDocumentTransformer(mockLogic, mockQuery, mockMarkingFunctions, mockResponseFactory, true);
+        SimpleEvent event = (SimpleEvent) transformer.transform(doc);
+        transformer.flush();
+
+        PowerMock.verifyAll();
+
         Assert.assertNotNull(event);
         Assert.assertEquals(1, event.getFields().size());
         Assert.assertEquals("field1", event.getFields().get(0).getName());
@@ -130,7 +189,7 @@ public class JsonDocumentTransformerTest { // extends EasyMockSupport {
         fieldMap.put("field2", fieldList);
         Document d = new Document(key,true);
         SerializedDocument doc = new SerializedDocument(d);
-        basicExpects(d, key, entry);
+        basicExpects(d, key, entry, 2);
         
         PowerMock.replayAll();
         
@@ -140,6 +199,34 @@ public class JsonDocumentTransformerTest { // extends EasyMockSupport {
         
         PowerMock.verifyAll();
         
+        Assert.assertNotNull(event);
+        Assert.assertEquals(1, event.getFields().size());
+        Assert.assertEquals("field1", event.getFields().get(0).getName());
+        Assert.assertEquals("5", event.getFields().get(0).getValueString());
+    }
+
+    @Test
+    public void transform_primaryEmptySecondarySetTestJson() throws MarkingFunctions.Exception {
+        Key key = new Key("shard", "dataType" + Constants.NULL + "uid");
+        Value value = new Value();
+        AbstractMap.SimpleEntry<Key,Value> entry = new AbstractMap.SimpleEntry<>(key, value);
+
+        Map<String,List<String>> fieldMap = new HashMap<>();
+        List<String> fieldList = Collections.EMPTY_LIST;
+        fieldMap.put("field2", fieldList);
+
+        JsonObject d = new JsonObject();
+
+        basicExpects(d, key, entry, 2);
+        JsonDocument doc = new JsonDocument(d,key.toThrift(),1);
+        PowerMock.replayAll();
+
+        transformer = new JsonDocumentTransformer(mockLogic, mockQuery, mockMarkingFunctions, mockResponseFactory, true);
+        transformer.setPrimaryToSecondaryFieldMap(fieldMap);
+        SimpleEvent event = (SimpleEvent) transformer.transform(doc);
+
+        PowerMock.verifyAll();
+
         Assert.assertNotNull(event);
         Assert.assertEquals(1, event.getFields().size());
         Assert.assertEquals("field1", event.getFields().get(0).getName());
@@ -159,7 +246,7 @@ public class JsonDocumentTransformerTest { // extends EasyMockSupport {
 
         Document d = new Document(key,true);
         SerializedDocument doc = new SerializedDocument(d);
-        basicExpects(d, key, entry);
+        basicExpects(d, key, entry, 2);
         
         PowerMock.replayAll();
         
@@ -169,6 +256,36 @@ public class JsonDocumentTransformerTest { // extends EasyMockSupport {
         
         PowerMock.verifyAll();
         
+        Assert.assertNotNull(event);
+        Assert.assertEquals(1, event.getFields().size());
+        Assert.assertEquals("field1", event.getFields().get(0).getName());
+        Assert.assertEquals("5", event.getFields().get(0).getValueString());
+    }
+
+    @Test
+    public void transform_primaryNoMatchSetTestJson() throws MarkingFunctions.Exception {
+        Key key = new Key("shard", "dataType" + Constants.NULL + "uid");
+        Value value = new Value();
+        AbstractMap.SimpleEntry<Key,Value> entry = new AbstractMap.SimpleEntry<>(key, value);
+
+        Map<String,List<String>> fieldMap = new HashMap<>();
+        List<String> fieldList = new ArrayList<>();
+        fieldList.add("field3");
+        fieldMap.put("field2", fieldList);
+
+        JsonObject d = new JsonObject();
+        basicExpects(d, key, entry, 2);
+
+        JsonDocument doc = new JsonDocument(d,key.toThrift(),1);
+
+        PowerMock.replayAll();
+
+        transformer = new JsonDocumentTransformer(mockLogic, mockQuery, mockMarkingFunctions, mockResponseFactory, true);
+        transformer.setPrimaryToSecondaryFieldMap(fieldMap);
+        SimpleEvent event = (SimpleEvent) transformer.transform(doc);
+
+        PowerMock.verifyAll();
+
         Assert.assertNotNull(event);
         Assert.assertEquals(1, event.getFields().size());
         Assert.assertEquals("field1", event.getFields().get(0).getName());
@@ -187,7 +304,7 @@ public class JsonDocumentTransformerTest { // extends EasyMockSupport {
         fieldMap.put("field2", fieldList);
         
         Document d = new Document(key,true);
-        basicExpects(d, key, entry);
+        basicExpects(d, key, entry, 2);
         SerializedDocument doc = new SerializedDocument(d);
         EasyMock.expect(mockResponseFactory.getField()).andReturn(new SimpleField());
         
@@ -232,10 +349,10 @@ public class JsonDocumentTransformerTest { // extends EasyMockSupport {
         Document d = new Document(key,true);
         d.put("field3", new Numeric("6", key, true));
         SerializedDocument doc = new SerializedDocument(d);
-        basicExpects(d, key, entry);
+        basicExpects(d, key, entry, 3);
 
         EasyMock.expect(mockResponseFactory.getField()).andReturn(new SimpleField());
-        EasyMock.expect(mockResponseFactory.getField()).andReturn(new SimpleField());
+        //EasyMock.expect(mockResponseFactory.getField()).andReturn(new SimpleField());
         
         PowerMock.replayAll();
         
@@ -246,7 +363,7 @@ public class JsonDocumentTransformerTest { // extends EasyMockSupport {
         PowerMock.verifyAll();
         
         Assert.assertNotNull(event);
-        Assert.assertEquals(3, event.getFields().size());
+        Assert.assertEquals(2, event.getFields().size());
         
         List<String> foundFields = new ArrayList<>(3);
         for (SimpleField field : event.getFields()) {
@@ -260,7 +377,7 @@ public class JsonDocumentTransformerTest { // extends EasyMockSupport {
         
         List<String> expectedFields = new ArrayList<>();
         expectedFields.add("field1");
-        expectedFields.add("field2");
+        expectedFields.add("field2"); // should not be there with this functionality  not enabled.
         expectedFields.add("field3");
         
         Assert.assertTrue(foundFields.containsAll(expectedFields));
