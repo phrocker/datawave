d8067432bb831b5956f3ecaf123ecd2ab5c2cf24
phrocker <phrocker@apache.org>
2022-05-21 10:20:17 +0000

    Update test and fix powermock issues

--- warehouse/query-core/src/test/java/datawave/query/QueryTestTableHelper.java
+++ warehouse/query-core/src/test/java/datawave/query/QueryTestTableHelper.java
@@ -72,14 +72,15 @@ public final class QueryTestTableHelper {
         
         createTables();
     }
-
-    public QueryTestTableHelper(AccumuloClient client, Logger log, RebuildingScannerTestHelper.TEARDOWN teardown, RebuildingScannerTestHelper.INTERRUPT interrupt)
-            throws AccumuloSecurityException, AccumuloException, TableExistsException, TableNotFoundException {
+    
+    public QueryTestTableHelper(AccumuloClient client, Logger log, RebuildingScannerTestHelper.TEARDOWN teardown,
+                    RebuildingScannerTestHelper.INTERRUPT interrupt) throws AccumuloSecurityException, AccumuloException, TableExistsException,
+                    TableNotFoundException {
         // create mock instance and connector
-
+        
         this.client = client;
         this.log = log;
-
+        
         createTables();
     }
     
