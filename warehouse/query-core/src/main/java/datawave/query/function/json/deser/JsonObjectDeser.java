f41b01518108ef0b56f7c20d9f43faffa594bd0f
Marc Parisi <phrocker@apache.org>
2023-05-21 06:32:47 -0400

    fix tests

--- warehouse/query-core/src/main/java/datawave/query/function/json/deser/JsonObjectDeser.java
+++ warehouse/query-core/src/main/java/datawave/query/function/json/deser/JsonObjectDeser.java
@@ -1,37 +1,13 @@
 package datawave.query.function.json.deser;
 
 
-import com.google.common.collect.Maps;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
-import com.google.gson.JsonSerializationContext;
-import datawave.data.type.BaseType;
-import datawave.data.type.NoOpType;
-import datawave.data.type.NumberType;
 import datawave.query.attributes.Attribute;
-import datawave.query.attributes.Attributes;
-import datawave.query.attributes.Document;
-import datawave.query.attributes.DocumentKey;
-import datawave.query.attributes.TimingMetadata;
-import datawave.query.attributes.TypeAttribute;
-import org.apache.accumulo.core.data.Key;
 import org.apache.log4j.Logger;
-
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Type;
-import java.math.BigDecimal;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Function;
 
 /**
  * Supports JSON object serialization and deserialization to and fro Documents.
  *
- * NIX
  */
 public class JsonObjectDeser extends JsonDeser{
     private static final Logger log = Logger.getLogger(JsonObjectDeser.class);
--- warehouse/query-core/src/main/java/datawave/query/function/serializer/JsonDocumentSerializer.java
+++ warehouse/query-core/src/main/java/datawave/query/function/serializer/JsonDocumentSerializer.java
@@ -1,6 +1,7 @@
 package datawave.query.function.serializer;
 
-import com.google.gson.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import datawave.query.attributes.Document;
 import datawave.query.function.json.deser.JsonDeser;
 
